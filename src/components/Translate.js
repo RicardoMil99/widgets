0801901e79419727dd6db8d5e1633876481c4b19
ricardo milhomens <ricardo.m.mil@hotmail.com>
2022-07-10 11:28:37 +0100

    Widgets Dropdown 2

---
+++ src/components/Translate.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Dropdown from './Dropdown';
+
+const Translate = () => {
+  return (
+    <div>
+      <Dropdown />
+    </div>
+  );
+};
+
+export default Translate;
